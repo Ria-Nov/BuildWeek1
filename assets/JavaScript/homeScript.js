00c22f947244d092a2f6c79de7fdd91c2f37e89d
“TudorBogdanC” <toriatteteo@gmail.com>
2023-11-15 12:50:43 +0100

    BuildWeek1 - javascript proceed

---
+++ assets/JavaScript/homeScript.js
@@ -0,0 +1,12 @@
+/* Fasi di sviluppo
+
+- dobbiamo checkare la check-box e al check attivare l'ancora del bottone per passare alla pagina successiva
+- attivazione bottone con classe disabled */
+
+
+let proceed = document.getElementById("proceed");
+
+function procedi() {
+    
+
+}

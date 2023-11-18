10780d39e27cbb611a4a1e20ab6214fe3d3765b5
martanagib <cv.marta1@gmail.com>
2023-11-18 17:40:09 +0100

    refactoring css

--- assets/JavaScript/feedbackscript.js
+++ assets/JavaScript/feedbackscript.js
@@ -1,33 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const stars = document.querySelectorAll('.star');
+document.addEventListener("DOMContentLoaded", function () {
+  const stars = document.querySelectorAll(".star");
 
-    stars.forEach(function (star) {
-        star.addEventListener('mouseover', function () {
-            resetStars();
-            const ratingValue = this.getAttribute('data-rating');
-            highlightStars(ratingValue);
-        });
-
-        // star.addEventListener('click', function () {
-        //     resetStars();
-        // });
-
-        star.addEventListener('click', function () {
-            // resetStars();
-            const ratingValue = this.getAttribute('data-rating');
-            highlightStars(ratingValue);
-        });
+  stars.forEach(function (star) {
+    star.addEventListener("mouseover", function () {
+      resetStars();
+      const ratingValue = this.getAttribute("data-rating");
+      highlightStars(ratingValue);
     });
 
-    function resetStars() {
-        stars.forEach(function (star) {
-            star.classList.remove('active');
-        });
-    }
+    // star.addEventListener('click', function () {
+    //     resetStars();
+    // });
 
-    function highlightStars(value) {
-        for (let i = 0; i < value; i++) {
-            stars[i].classList.add('active');
-        }
+    star.addEventListener("click", function () {
+      // resetStars();
+      const ratingValue = this.getAttribute("data-rating");
+      highlightStars(ratingValue);
+    });
+  });
+
+  function resetStars() {
+    stars.forEach(function (star) {
+      star.classList.remove("active");
+    });
+  }
+
+  function highlightStars(value) {
+    for (let i = 0; i < value; i++) {
+      stars[i].classList.add("active");
     }
+  }
 });

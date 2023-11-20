41b0d8528a90a13a7c86abd9c27ff7d844b3c3fd
Mickmart96 <micheleciceron@gmail.com>
2023-11-20 10:56:25 +0100

    changeultimate

--- assets/JavaScript/feedbackscript.js
+++ assets/JavaScript/feedbackscript.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-/*document.addEventListener('DOMContentLoaded', function () {
-    const stars = document.querySelectorAll('.star');
-
-    stars.forEach(function (star) {
-        star.addEventListener('mouseover', function () {
-            resetStars();
-            const ratingValue = this.getAttribute('data-rating');
-            highlightStars(ratingValue);
-        });
-
-        // star.addEventListener('click', function () {
-        //     resetStars();
-        // });
-
-        star.addEventListener('click', function () {
-            // resetStars();
-            const ratingValue = this.getAttribute('data-rating');
-            selectedRating.innerHTML = 'Hai valutato con ' + ratingValue + ' stella(e).';
-            resetStars();
-            highlightStars(ratingValue);
-        });
-=======
 document.addEventListener("DOMContentLoaded", function () {
   const stars = document.querySelectorAll(".star");
 
@@ -29,7 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
       resetStars();
       const ratingValue = this.getAttribute("data-rating");
       highlightStars(ratingValue);
->>>>>>> 74cf9056b102ab47022690e150856b8d58634077
     });
 
     // star.addEventListener('click', function () {
@@ -53,45 +29,5 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < value; i++) {
       stars[i].classList.add("active");
     }
-<<<<<<< HEAD
-});*/
-document.addEventListener('DOMContentLoaded', function () {
-    const stars = document.querySelectorAll('.star');
-    const selectedRating = document.getElementById('selectedRating');
-
-    stars.forEach(function (star) {
-        star.addEventListener('mouseover', function () {
-            const ratingValue = this.getAttribute('data-rating');
-            highlightStars(ratingValue);
-        });
-
-        star.addEventListener('mouseout', function () {
-            resetStars();
-        });
-
-        star.addEventListener('click', function () {
-            const ratingValue = this.getAttribute('data-rating');
-           
-            highlightStars(ratingValue);
-            
-            this.classList.add('selected'); // Aggiungi la classe 'selected' alla stella cliccata
-            //resetStars();        
-        });
-    });
-
-    function resetStars() {
-        stars.forEach(function (star) {
-            star.classList.remove('active');
-        });
-    }
-
-    function highlightStars(value) {
-        for (let i = 0; i < value; i++) {
-            stars[i].classList.add('active');
-        }
-    }
-});
-=======
   }
 });
->>>>>>> 74cf9056b102ab47022690e150856b8d58634077
